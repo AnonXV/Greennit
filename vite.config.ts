d63f5eaae56c8506524a94c31ff98b6f87197073
Augustine <augustinelbecker@gmail.com>
2026-01-09 17:05:34 -0600

    configure github pages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
-import react from "@vitejs/plugin-react"
+import { defineConfig } from 'vitest/config'
+import react from '@vitejs/plugin-react'
 import * as path from "node:path"
-import { defineConfig } from "vitest/config"
 import packageJson from "./package.json" with { type: "json" }
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Greennit/",
+  base: '/reddit-app/',
   plugins: [react()],
 
   server: {
