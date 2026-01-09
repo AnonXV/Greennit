cd055621ead44039e5d6e73a93f6f87e1da13aaa
Augustine <augustinelbecker@gmail.com>
2026-01-09 17:20:20 -0600

    vite to vitest

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,8 @@ import packageJson from "./package.json" with { type: "json" }
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/reddit-app/',
+  base: '/Greennit/',
+  build: { outDir: 'docs' },
   plugins: [react()],
 
   server: {
